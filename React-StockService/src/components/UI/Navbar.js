79d67962aa7e91cae0cfed2ea25de2d9a83a45e3
Aravamudhan Pavithra <Aravamudhan.Pavithra@craneww.com>
2021-07-31 22:10:15 +0530

    Autocomplete Changes

--- React-StockService/src/components/UI/Navbar.js
+++ React-StockService/src/components/UI/Navbar.js
@@ -1,46 +1,126 @@
 import AppBar from '@material-ui/core/AppBar';
+import React, { useEffect,useState } from 'react';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { TextField } from '@material-ui/core';
 import { makeStyles } from "@material-ui/core/styles";
-import { useHistory } from 'react-router-dom';
 
-import {useContext} from 'react';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { useContext } from 'react';
 import AuthContext from '../../store/auth-context';
+import { STOCK_SERVICE_API_URL } from '../../utils/Constants';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-   
+
   },
   menuButton: {
     marginRight: theme.spacing(2),
   },
   title: {
     flexGrow: 1,
-    textAlign:"left",
-    marginLeft:theme.spacing(3)
+    textAlign: "left",
+    marginLeft: theme.spacing(3)
   },
 }));
 
-const NavBar= ()=> {
+const NavBar = () => {
   const classes = useStyles();
-  
-  
+  const [open, setOpen] = useState(false);
+  const [options, setOptions] = useState([]);
+  const loading = open && options.length === 0;
+  const [autoSuggestVal,setAutoSuggestVal] = useState();
+  const [inputValue, setInputValue] = React.useState('');
+
   const context = useContext(AuthContext);
-  
+  useEffect(async () => {
+    let active = true;
+   
+
+
+    if (loading && context.isLoggedIn && inputValue!=='') {
+      const response = await fetch(
+        STOCK_SERVICE_API_URL + 'api/autosuggest?region=US&q='+inputValue, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + localStorage.getItem("jwtToken")
+        }
+      }
+      );
+      if (!response.ok) {
+        throw new Error('Could not fetch chartDetails!');
+
+      }
+
+      const suggestions = await response.json();
+      console.log(suggestions.quotes);
+
+      if (active) {
+        setOptions(suggestions.quotes.map(x => x.shortname));
+      }
+    }
+    return () => {
+      active = false;
+    };
+
+  }, [loading, context.isLoggedIn,inputValue]);
+  useEffect(() => {
+    if (!open) {
+      setOptions([]);
+    }
+  }, [open]);
 
   return (
     <div className={classes.root}>
       <AppBar position="static" className={classes.appBar}>
         <Toolbar>
-        
+
           <Typography variant="h6" className={classes.title}>
             Stock Details
           </Typography>
-          {context.isLoggedIn  && <TextField placeholder="Search for Symbols"/>}
-          {context.isLoggedIn  && <Button color="inherit" onClick={context.logout}>Logout</Button>}
-         
+          {context.isLoggedIn && <Autocomplete
+            id="asynchronous-demo"
+            style={{ width: 300 }}
+            open={open}
+            onOpen={() => {
+              setOpen(true);
+            }}
+            onClose={() => {
+              setOpen(false);
+            }}
+            value={inputValue}
+            onChange={(event, newValue) => {
+              setAutoSuggestVal(newValue);
+            }}
+            onInputChange={(event, newInputValue) => {
+              setInputValue(newInputValue);
+            }}
+            getOptionSelected={(option, value) => option === value}
+            getOptionLabel={(option) => option}
+            options={options}
+            loading={loading}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Symbols"
+                variant="outlined"
+              
+                InputProps={{
+                  ...params.InputProps,
+                  endAdornment: (
+                    <React.Fragment>
+                      {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                      {params.InputProps.endAdornment}
+                    </React.Fragment>
+                  ),
+                }}
+              />
+            )}
+          />}
+          {context.isLoggedIn && <Button color="inherit" onClick={context.logout}>Logout</Button>}
+
         </Toolbar>
       </AppBar>
     </div>
